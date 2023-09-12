4bad776885a8ef356971f99c733f54a1f3c229ed
allisoneglow <allisoneglow@outlook.com>
2023-09-12 12:43:25 -0400

    new footer file

---
+++ src/components/footer.js
@@ -0,0 +1,26 @@
+
+import React from "react";
+import logo from '../images/logos/logo.png';
+import '../App.css';
+import { Navbar, Container } from "react-bootstrap";
+
+const Footer = () => {
+
+  return (
+    <>
+      <Navbar>
+        <Container>
+            <img
+              src={logo}
+              width="100"
+              height="38"
+            />
+        </Container>
+      </Navbar>
+      
+
+    </>
+  )
+}
+
+export default Footer;
