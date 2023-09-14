acffa74cacadf5f9bfb68f24d943201adb374816
allisoneglow <allisoneglow@outlook.com>
2023-09-14 11:33:14 -0400

    Updates

---
+++ precache-manifest.b40854e84af96264cfdee4c8e298cd71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d719149993bd2d19b4c2",
+    "url": "/demo/static/css/main.bc88004e.chunk.css"
+  },
+  {
+    "revision": "d719149993bd2d19b4c2",
+    "url": "/demo/static/js/main.d7191499.chunk.js"
+  },
+  {
+    "revision": "46c3c26490a9244cbd4e",
+    "url": "/demo/static/js/1.46c3c264.chunk.js"
+  },
+  {
+    "revision": "3486bb47d5b32df8eb1c",
+    "url": "/demo/static/js/2.3486bb47.chunk.js"
+  },
+  {
+    "revision": "9b38e8f8528abbe38357",
+    "url": "/demo/static/js/runtime~main.9b38e8f8.js"
+  },
+  {
+    "revision": "b23eb8db37827fe883938a930c03ccfd",
+    "url": "/demo/static/media/credit-card.b23eb8db.png"
+  },
+  {
+    "revision": "94a5064c8eeffde8c65a00b87c4d7da8",
+    "url": "/demo/static/media/rewardsCard.94a5064c.png"
+  },
+  {
+    "revision": "6ffd9ae81434270c9540810a24a239fb",
+    "url": "/demo/index.html"
+  }
+];
