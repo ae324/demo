c1b9933fa9296ef8d9dcfeb9939829c532ec9d7c
allisoneglow <allisoneglow@outlook.com>
2023-09-14 11:58:18 -0400

    code clean up

--- src/pages/initialpage.js
+++ src/pages/initialpage.js
@@ -1,11 +1,7 @@
 import React from "react";
-import card1 from "../images/icons/deposit.png";
-import card2 from "../images/icons/findatm.png";
-import card3 from "../images/icons/sendZelle.png";
-import rewardsCard from "../images/icons/rewardsCard.png";
-import { Row, Col, Button, Container } from "react-bootstrap";
+
+import {Button } from "react-bootstrap";
 import '../wallet.css';
-import Wallet from "./wallet";
 
 const InitialPage = () => {
 
